abd8f4c476c7deb0f15262de4238533367127118
k-ris <christkuzanuka@gmail.com>
2023-09-20 19:27:32 +0100

    Partie Déconnexion

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if($_SESSION['admin']==null AND $_SESSION['manager']==null AND $_SESSION['dealer']==null){
-    header("Location: login.php");
+    header("Location: ../login.php");
 }
 $content = "Hello ". $_SESSION['admin'];
 require '../partials/app.php';
--- partials/header.php
+++ partials/header.php
@@ -168,7 +168,7 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <img src="../assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2">Admin</span>
           </a><!-- End Profile Iamge Icon -->
 
@@ -205,17 +205,14 @@
             </li>
 
             <li>
-                <form action="" method="post">
-                    <button class="dropdown-item d-flex align-items-center" type="submit">
-                        <i class="bi bi-box-arrow-right"></i>
-                        <span>Sign Out</span>
-                    </button>
-                </form>
+                <a class="dropdown-item d-flex align-items-center" href="../logout.php">
+                    <i class="bi bi-box-arrow-right"></i>
+                    <span>Se déconnecter</span>
+                </a>
             </li>
 
           </ul>
         </li>
-
       </ul>
     </nav>
 
